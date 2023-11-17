df4ff0a921e734aa8c814373ac93592aa0e10cb9
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-11-17 14:22:57 +0200

    done with controllers

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+require('./config/database/connection');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+const venueRoutes = require('./routes/venueRoutes');
+const eventRoutes = require('./routes/eventRoutes');
+const reservationRoutes = require('./routes/reservationRoutes')
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/user', userRoutes);
+app.use('/venue', venueRoutes);
+app.use('/event', eventRoutes);
+app.use('/reservation', reservationRoutes);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
